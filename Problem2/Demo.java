5a9c2477255ca856d5e577bcdbaef0b192057504
William Moss <you@example.com>
2024-11-20 22:57:02 -0330

    Updated

--- Problem2/Demo.java
+++ Problem2/Demo.java
@@ -2,11 +2,11 @@ package Problem2;
 
 public class Demo {
     public static void main(String[] args) {
-        // Create a Point object
+        // Create the Point object
         Point point = new Point(1.0f, 2.0f);
         System.out.println("Point: " + point);
 
-        // Create a MovablePoint object
+        // Create the MovablePoint object
         MovablePoint movablePoint = new MovablePoint(1.0f, 2.0f, 0.5f, 1.5f);
         System.out.println("MovablePoint before moving: " + movablePoint);
 
